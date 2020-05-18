3c0e543d5e9abe2c329fb75494d2c0d312a3abe6
Jared  E <jared.epps322@gmail.com>
2020-05-18 00:59:04 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer 
-  extend MetaDancing 
-end 
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   
   
   
